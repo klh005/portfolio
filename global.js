406e4606f149bf6228398e7c0b34d32a9cf6edff
klh005 <klh005@ucsd.edu>
2025-01-24 11:11:11 -0800

    lab3.2

--- global.js
+++ global.js
@@ -22,10 +22,10 @@ currentLink?.classList.add('current');
 const ARE_WE_HOME = document.documentElement.classList.contains('home');
 
 let pages = [
-  { url: '', title: 'Home' },
-  { url: '/projects/', title: 'Projects' },
-  { url: '/contact/', title: 'Contact' },
-  { url: '/resume/', title: 'Resume' },
+  { url: 'index.html', title: 'Home' },
+  { url: '/projects/index.html', title: 'Projects' },
+  { url: '/contact/index.html', title: 'Contact' },
+  { url: '/resume/index.html', title: 'Resume' },
   { url: 'https://github.com/klh005', title: 'Github' }
 ];
 
