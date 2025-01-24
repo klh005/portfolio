2c7a17c89385d6cf83aa8f79289c6623afd1c6b7
klh005 <klh005@ucsd.edu>
2025-01-24 11:14:38 -0800

    lab3.3

--- global.js
+++ global.js
@@ -22,10 +22,10 @@ currentLink?.classList.add('current');
 const ARE_WE_HOME = document.documentElement.classList.contains('home');
 
 let pages = [
-  { url: 'index.html', title: 'Home' },
-  { url: '/projects/index.html', title: 'Projects' },
-  { url: '/contact/index.html', title: 'Contact' },
-  { url: '/resume/index.html', title: 'Resume' },
+  { url: '', title: 'Home' },
+  { url: 'projects/', title: 'Projects' },
+  { url: 'contact/', title: 'Contact' },
+  { url: 'resume/', title: 'Resume' },
   { url: 'https://github.com/klh005', title: 'Github' }
 ];
 
@@ -35,7 +35,7 @@ document.body.prepend(nav);
 for (let p of pages) {
   let url = p.url;
   let title = p.title;
-  url = !ARE_WE_HOME && !url.startsWith('http') ? '../' + url : url;
+  url = !ARE_WE_HOME && !url.startsWith('http') ? '../portfolio/' + url : url;
   let a = document.createElement('a');
   a.href = url;
   a.textContent = title;
